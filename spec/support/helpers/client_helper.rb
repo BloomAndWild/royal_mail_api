b23ca80b818a800386e3e95812c7931f2ab92dc9
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2017-01-06 11:11:20 +0000

    Fixing file location

--- spec/support/helpers/client_helper.rb
+++ spec/support/helpers/client_helper.rb
@@ -4,8 +4,8 @@ def configure_client
     config.username = ENV['RM_USERNAME']
     config.password = ENV['RM_PASSWORD']
     config.adapter = :httpclient
-    config.shipping_wsdl = File.join(RoyalMailApi.root, 'tmp', 'wsdl', 'ShippingAPI_V2_0_9.wsdl')
-    config.tracking_wsdl = File.join(RoyalMailApi.root, 'tmp', 'wsdl', 'Tracking_API_V1.2.wsdl')
+    config.shipping_wsdl = File.join(RoyalMailApi.root, 'lib', 'wsdl', 'ShippingAPI_V2_0_9.wsdl')
+    config.tracking_wsdl = File.join(RoyalMailApi.root, 'lib', 'wsdl', 'Tracking_API_V1.2.wsdl')
     config.shipping_endpoint = 'https://api.royalmail.net/shipping/v2'
     config.tracking_endpoint = 'https://api.royalmail.com/tracking'
     config.logger = Logger.new(STDOUT)
