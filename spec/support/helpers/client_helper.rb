2087830dce1346cba55b620a0a29241b73d870e7
Srikanth Kunkulagunta <srikanth.kunkulagunta@gmail.com>
2017-02-17 11:21:56 +0000

    updated tracking endpoint in spec

--- spec/support/helpers/client_helper.rb
+++ spec/support/helpers/client_helper.rb
@@ -7,7 +7,7 @@ def configure_client
     config.shipping_wsdl = File.join(RoyalMailApi.root, 'lib', 'wsdl', 'ShippingAPI_V2_0_9.wsdl')
     config.tracking_wsdl = File.join(RoyalMailApi.root, 'lib', 'wsdl', 'Tracking_API_V1.2.wsdl')
     config.shipping_endpoint = 'https://api.royalmail.net/shipping/v2'
-    config.tracking_endpoint = 'https://api.royalmail.com/tracking'
+    config.tracking_endpoint = 'https://api.royalmail.net/tracking'
     config.logger = Logger.new(STDOUT)
     config.logger.level = 0
   end
